34a024a8caaedc87584d8a8d8b65abdf1956ff38
vineetha <vineethaveerapu4@gmail.com>
2024-11-28 21:58:10 +0100

    add scroll to details view

--- js/sidebar.js
+++ js/sidebar.js
@@ -126,7 +126,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const container = document.querySelector(".container");
 
   animals.forEach(({ category, pageLink, entries }) => {
-    // if page slug matchtes category we show more else we return for home page we show all
     const isCategoryPage = window.location.pathname.includes(
       category.toLowerCase()
     );
@@ -169,7 +168,15 @@ document.addEventListener("DOMContentLoaded", function () {
         heading.appendChild(image);
 
         heading.addEventListener("click", function () {
-          // remove active class from all headings
+          const animalDescriptionElement =
+            document.getElementById("animal-description");
+          if (animalDescriptionElement) {
+            animalDescriptionElement.scrollIntoView({
+              behavior: "smooth",
+              block: "start"
+            });
+          }
+
           const activeHeading = document.querySelector(".animal-name.active");
           if (activeHeading && activeHeading !== heading) {
             activeHeading.classList.remove("active");
